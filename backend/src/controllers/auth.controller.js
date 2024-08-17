92def08da99c687eb1a24ade541c97e660e1fa3b
CrisNK <cristobalalarcon22@gmail.com>
2024-08-16 22:48:47 -0400

    implementación de inicio de sesión - falta agregar una sesión de express

---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,28 @@
+import Usuarios from '../models/user.model.js';
+import bcrypt from 'bcryptjs';
+
+export async function iniciarSesion(req, res) {
+    try {
+        const { rut, contrasena } = req.body;
+
+        const usuarioEncontrado = await Usuarios.findOne({ where: { rut: rut }});
+        if (!usuarioEncontrado) {
+            return res.status(404).json({ message: 'Usuario no encontrado' });
+        }
+
+        const rolesPermitidos = ['Administrador', 'Profesor'];
+        if (!rolesPermitidos.includes(usuarioEncontrado.rol)) {
+            return res.status(403).json({ message: 'No tienes permitido acceder aqui' });
+        }
+
+        const contrasenaValida = await bcrypt.compare(contrasena, usuarioEncontrado.contrasena);
+        if (contrasenaValida) {
+            res.status(200).json({ message: 'Inicio de sesión exitoso' });
+        } else {
+            res.status(401).json({ message: 'Contraseña incorrecta' });
+        }
+    } catch (error) {
+        console.error('Error en auth.controller.js -> iniciarSesion():', error);
+        res.status(500).json({ message: error });
+    }
+}
--- backend/src/routes/index.routes.js
+++ backend/src/routes/index.routes.js
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 import userRoutes from './user.routes.js';
+import authRoutes from './auth.routes.js';
 
 const router = Router();
 
 router.get('/');
+router.use('/autenticacion', authRoutes);
 router.use('/usuario', userRoutes);
 
 export default router;
