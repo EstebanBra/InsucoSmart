a7afbf8a3bf2e116de75a394857808443e694f56
CrisNK <cristobalalarcon22@gmail.com>
2024-08-18 14:26:45 -0400

    componente de marcarAtraso fijado en ruta principal

--- frontend/src/AppRouter.jsx
+++ frontend/src/AppRouter.jsx
@@ -1,12 +1,13 @@
 import { Routes, Route } from 'react-router-dom';
 import Login from './pages/Login.jsx';
 import MarcarAtraso from './pages/MarcarAtraso.jsx';
+import NavBar from './components/NavBar.jsx';
+
 export default function AppRouter() {
   return (
     <Routes>
-      <Route path='/' />
+      <Route path='/' element={<MarcarAtraso />}/>
       <Route path='/auth/login' element={<Login />} />              
-      <Route path='/marcarAtraso' element={<MarcarAtraso />} />
     </Routes>
   );
 }
