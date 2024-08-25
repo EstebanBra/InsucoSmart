359f4f8cc7b5e69e0889c732aa251ee0d04f9cf5
Diegariio <diegovargasag@gmail.com>
2024-08-25 03:17:38 -0400

    Add: funcionalidad de generarJustificativo

--- backend/src/routes/atrasos.routes.js
+++ backend/src/routes/atrasos.routes.js
@@ -1,9 +1,12 @@
 // routes/alumno.route.js
 import { Router } from 'express';
 import { registrarAtraso } from '../controllers/atrasos.controller.js';
+import { generarJustificativo } from '../controllers/justificativo.controller.js';   
+import { upload } from '../config/googleService.js'; // Importa 'upload'
 
 const router = Router();
 
 router.post('/obtener', registrarAtraso);
+router.post('/upload-justificativo', upload.single('file'), generarJustificativo); // Agregar el middleware de upload
 
 export default router;
