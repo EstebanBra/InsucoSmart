0de6c88c45590aa3d8e6696e6ec2b25a130d239c
Diegariio <diegovargasag@gmail.com>
2024-09-01 00:54:04 -0400

    FIX: modelos nuevos soluci[on provisoria

--- backend/src/models/atrasos.model.js
+++ backend/src/models/atrasos.model.js
@@ -1,12 +1,17 @@
 import { DataTypes } from 'sequelize';
 import sequelize from '../config/configDB.js';
 import Usuario from '../models/user.model.js';
-
+import Imparte from '../models/imparte.model.js';
+import Materia from '../models/materia.model.js';
 const Atraso = sequelize.define('Atraso', {
     atraso_id: {
         type: DataTypes.INTEGER,
         autoIncrement: true,
         primaryKey: true,
+    },
+    estado: { //0 inactivo, 1 activo
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
     },
     fecha: {
         type: DataTypes.DATEONLY,
@@ -28,12 +33,14 @@ const Atraso = sequelize.define('Atraso', {
       type: DataTypes.STRING,
       references: {
         model: Usuario,
-        key: 'rut'
+        key: 'rut',
       },
     },
 }, {
     tableName: 'ATRASO',
     timestamps: false,
+    Imparte,
+    Materia
 });
 
   Atraso.belongsTo(Usuario, { foreignKey: 'rutpersona' });
