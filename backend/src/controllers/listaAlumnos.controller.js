1d8cfba8d264149a40f67d255e54f2c87cc6774e
Diegariio <diegovargasag@gmail.com>
2024-09-04 01:35:11 -0400

    ADD: Función para tabla de atrasos

--- backend/src/controllers/listaAlumnos.controller.js
+++ backend/src/controllers/listaAlumnos.controller.js
@@ -1,38 +1,122 @@
 import Usuario from '../models/user.model.js';
 import Atraso from '../models/atrasos.model.js';
 import Curso from '../models/curso.model.js';
+import Imparte from '../models/imparte.model.js';
+import Materia from '../models/materia.model.js';
+import Justificativo from '../models/justificativo.model.js';
+
+import { Op } from 'sequelize';
 
 
 export async function obtenerAlumnosConAtrasos(req, res) {
   try {
-    // Obtiene todos los registros de Persona con el rol de 'alumno'
-    const alumnos = await Usuario.findAll({
-      where: { rol: 'Alumno' }, // Filtra por el rol 'alumno'
-      attributes: ['nombre', 'rut'], // Incluye estos atributos en el resultado
-      include: [{
-        model: Atraso, // Incluye la relación con Atraso
-        attributes: [] // No es necesario incluir atributos específicos aquí
-      },{
-        model: Curso,
-        attributes: ['numero_curso']
-      }]
-    });
 
-    const result = await Promise.all(alumnos.map(async alumno => {
-      const totalAtrasos = await Atraso.count({
-        where: { rutpersona: alumno.rut, estado: true }
+    const resultados = await Atraso.findAll({
+      attributes: ['estado', 'hora', 'fecha', 'atraso_id'],
+      where: {
+        estado: true  // Filtra por estado igual a true
+      },
+      include: [
+        {
+          model: Usuario,
+          attributes: ['rut', 'nombre'],
+          include: [
+            {
+              model: Curso,
+              attributes: ['numero_curso', 'curso_id'],
+            },
+          ],
+        },
+      ],
+    });
+    //console.log(resultados);
+
+    const resultadosImparte = [];
+
+    for (const resultado of resultados) {
+        // Asegúrate de que estás accediendo correctamente a los datos anidados
+      const usuario = resultado?.dataValues?.Usuario?.dataValues;
+      const curso = usuario?.Curso?.dataValues;
+      //console.log(usuario);
+      //console.log(curso);
+
+      const fecha = resultado.fecha;
+      const horaAtraso =  resultado.hora;
+      console.log(fecha);
+      console.log(horaAtraso);
+
+      //Cambiar la fecha a Dia de la semana
+      const diasSemana = ['Domingo', 'Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado'];
+      const diaSemana = diasSemana[(new Date(fecha).getDay() + 1) % 7];
+      console.log(diaSemana);
+
+      const imparte = await Imparte.findOne({
+        where: {
+          curso_id: curso.curso_id,
+          dia: diaSemana,
+          [Op.and]: [
+            {
+              hora_inicio: {
+                [Op.lte]: horaAtraso
+              }
+            },
+            {
+              hora_fin: {
+                [Op.gte]: horaAtraso
+              }
+            }
+          ]
+        },
+        attributes: ['materia_id', 'curso_id'] // Asegúrate de incluir curso_id en los atributos
       });
 
-      return {
-        nombre: alumno.nombre,
-        rut: alumno.rut,
-        curso: alumno.Curso.numero_curso,
-        totalatrasos: totalAtrasos // Total de atrasos por rut
-      };
-    }));
+      const justificativo = await Justificativo.findOne({
+        where: {atraso_id: resultado.atraso_id},
+        attributes: ['estado']
+       });
 
-    // Envía la respuesta en formato JSON
-    res.json(result);
+       let Justificado;
+       if (!justificativo) {
+         Justificado = 'no justificado';
+       } else {
+         Justificado = justificativo.estado;
+       }
+
+
+      if (imparte) {
+        const materia = await Materia.findOne({
+          where: {
+            materia_id: imparte.materia_id
+          },
+          attributes: ['nombre_materia']
+        });
+    
+        resultadosImparte.push({
+          fechaAtraso: resultado.fecha,
+          horaAtraso: resultado.hora,
+          numeroCurso: resultado.Usuario.Curso.numero_curso,
+          nombreMateria: materia.nombre_materia,
+          rutUsuario: resultado.Usuario.rut,
+          nombreUsuario: resultado.Usuario.nombre,
+          justificado: Justificado
+
+        });
+      } else {
+        resultadosImparte.push({
+          fechaAtraso: resultado.fecha,
+          horaAtraso: resultado.hora,
+          estado: resultado.estado,
+          nombreMateria: null,
+          rutUsuario: resultado.Usuario.rut,
+          nombreUsuario: resultado.Usuario.nombre,
+          numeroCurso: resultado.Usuario.Curso.numero_curso,
+          justificado: Justificado
+
+        });
+      }
+    }
+    res.json(resultadosImparte);
+
   } catch (error) {
     console.error('Error al encontrar alumnos:', error);
     res.status(500).json({ message: 'Error alumnos' });
